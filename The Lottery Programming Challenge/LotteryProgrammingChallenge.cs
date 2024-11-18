136e8c09db53b462174c301dcf76f743df4804c4
Dan Roberts <DRoberts@ipipeline.com>
2024-11-18 10:23:58 +0000

    Comments

--- The Lottery Programming Challenge/LotteryProgrammingChallenge.cs
+++ The Lottery Programming Challenge/LotteryProgrammingChallenge.cs
@@ -19,6 +19,7 @@ namespace The_Lottery_Programming_Challenge
       InitializeComponent();
     }
     string myNumbers = "";
+    //Use variables to allow easy scalability 
     int maxNumbers = 6;
     int minRange = 1;
     int maxRange = 49;
@@ -29,22 +30,27 @@ namespace The_Lottery_Programming_Challenge
       Random r = new Random();
       string generatedNumbers = "";
 
+      //While loop to reach the max numbers (6 default, 7 with "bonus ball"
       while(list.Count < maxNumbers)
       {
+        //Generate random number within range, use of variables in case 1-49 changes
         int randomNumber = r.Next(minRange, maxRange);
+        //If the number is not already in the list, add it, otherwise loop again
         if (!list.Contains(randomNumber)) 
         { 
           list.Add(randomNumber);
         }
       }
-
+      //Numerically sort
       list.Sort();
 
+      //Loop to add list items to a variable, with a space between each number
       for(int i = 0; i < list.Count; i++)
       {
         generatedNumbers += list[i] + " ";
       }
 
+      //Final step to add numbers to the tech box
       LotteryNumbers.Text = generatedNumbers;
       LotteryNumbers.SelectAll();
       LotteryNumbers.SelectionAlignment = HorizontalAlignment.Center;
